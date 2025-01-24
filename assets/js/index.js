e1a66ae04b477fec3141030ae62f14c32ec69815
wendy munyasi <wendymunyasi@gmail.com>
2025-01-24 15:40:30 +0300

    Remove the url name because it was a bad idea

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,10 +16,6 @@ navLinks.forEach(link => {
         behavior: 'smooth', // Smooth scrolling
         block: 'start' // Align to the top of the section
       });
-
-      // Update the URL path without reloading the page
-      const newPath = `/${targetClass}`;
-      window.history.pushState(null, null, newPath);
     } else {
       console.error(`Section with class "${targetClass}" not found.`);
     }
